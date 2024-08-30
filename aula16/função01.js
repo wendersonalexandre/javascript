8acf44826eb2893defb34c1cd33cd2b6573427e4
wendersonalexandre02 <136288467+wendersonalexandre@users.noreply.github.com>
2024-08-29 21:38:52 -0300

    Aprendendo mais sobre funçoes

---
+++ aula16/função01.js
@@ -0,0 +1,12 @@
+function parimp(n){
+    if(n%2 == 0){
+        return 'par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+/* console.log(parimp(9)) */
+
+let res = parimp(17)
+console.log(res)
